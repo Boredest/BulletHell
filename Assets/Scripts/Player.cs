9199a21a28f3eca54bc84c4c28cb26641522efb6
Boredest <hodge195@gmail.com>
2022-05-27 23:09:44 -0700

    Adjusted player sprite and added basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb2d;
+    public float speed = 5.0f;
+
+    private float leftBoundOffset;
+    private float rightBoundOffset;
+    private void Awake()
+    {
+       
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            this.transform.position += Vector3.left * this. speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
     }
 }
