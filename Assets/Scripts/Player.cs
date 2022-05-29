d8a483499744a82bdb9b8de776ad04e32ef2c077
Boredest <hodge195@gmail.com>
2022-05-29 16:02:34 -0700

    Updated player script for movement and clamped screen boundaries.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,30 +5,73 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb2d;
-    public float speed = 5.0f;
+    private Vector3 leftEdge;
+    private Vector3 rightEdge;
+    private Vector3 topEdge;
+    private Vector3 bottomEdge;
+    private Vector3 position;
+    public SpriteRenderer sprRenderer;
+    private float playerHalfWidth;
+    
 
-    private float leftBoundOffset;
-    private float rightBoundOffset;
+    public float speed = 5.5f;
+
+     
     private void Awake()
     {
-       
-    }
+        sprRenderer.GetComponent<SpriteRenderer>();
+        
+    }//Awake
     void Start()
     {
-        
-    }
+        Vector3 position = transform.position;
+        playerHalfWidth = sprRenderer.bounds.size.x / 2;
+        leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        topEdge = Camera.main.ViewportToWorldPoint(Vector3.up);
+        bottomEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+
+    }//Start
 
 
     private void Update()
     {
+        position = transform.position;
+        
+      
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
         {
-            this.transform.position += Vector3.left * this. speed * Time.deltaTime;
+            position.x -=  this.speed * Time.deltaTime;
+          
         }
 
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+            position.x +=  this.speed * Time.deltaTime;
         }
-    }
+
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            position.y += this.speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            position.y -= this.speed * Time.deltaTime;
+        }
+
+     
+        position.x = Mathf.Clamp(position.x, leftEdge.x + playerHalfWidth, rightEdge.x - playerHalfWidth);
+        position.y = Mathf.Clamp(position.y, bottomEdge.y + playerHalfWidth, topEdge.y - playerHalfWidth);
+
+        transform.position = position;
+
+      
+
+    }//Update
+
+    private void Shoot()
+    {
+        //todo
+    }//Shoot
 }
