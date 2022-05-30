8fc5ced4272bbf756587389ea1499bef8d3a7f6a
Boredest <hodge195@gmail.com>
2022-05-29 18:02:40 -0700

    Added bullet script and shooting mechanic to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,17 @@ public class Player : MonoBehaviour
     private Vector3 topEdge;
     private Vector3 bottomEdge;
     private Vector3 position;
+    private Vector3 spawnOffSet;
+
+    private bool isShooting;
+    private float shootDelay = 0.25f;
+   
+    
     public SpriteRenderer sprRenderer;
     private float playerHalfWidth;
-    
-
     public float speed = 5.5f;
+
+    public Projectile bulletPrefab;
 
      
     private void Awake()
@@ -30,24 +36,29 @@ public class Player : MonoBehaviour
         rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
         topEdge = Camera.main.ViewportToWorldPoint(Vector3.up);
         bottomEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        isShooting = false;
+        spawnOffSet = new Vector3(this.transform.position.x, this.transform.position.y + playerHalfWidth, 0.0f);
 
     }//Start
 
 
     private void Update()
     {
-        position = transform.position;
-        
-      
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
+        checkInput();
+   
+    }//Update
+
+    private void checkInput()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            position.x -=  this.speed * Time.deltaTime;
-          
+            position.x -= this.speed * Time.deltaTime;
+
         }
 
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            position.x +=  this.speed * Time.deltaTime;
+            position.x += this.speed * Time.deltaTime;
         }
 
         else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
@@ -60,18 +71,35 @@ public class Player : MonoBehaviour
             position.y -= this.speed * Time.deltaTime;
         }
 
-     
+        if (Input.GetKeyDown(KeyCode.Space) && isShooting == false)
+        {
+            Shoot();
+        }
+
+
         position.x = Mathf.Clamp(position.x, leftEdge.x + playerHalfWidth, rightEdge.x - playerHalfWidth);
         position.y = Mathf.Clamp(position.y, bottomEdge.y + playerHalfWidth, topEdge.y - playerHalfWidth);
 
         transform.position = position;
-
-      
-
-    }//Update
+    }
 
     private void Shoot()
     {
-        //todo
+        if (!isShooting)
+        {
+           Instantiate(this.bulletPrefab, this.transform.position + spawnOffSet, Quaternion.identity);
+            isShooting = true;
+        }
+
+        if (isShooting)
+        {
+            StartCoroutine(ResetShot());
+        }
     }//Shoot
+
+    IEnumerator ResetShot()
+    {
+        yield return new WaitForSeconds(shootDelay);
+        isShooting = false;
+    }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public Vector3 direction;
+
+    void Update()
+    {
+        this.transform.position += Vector3.up * speed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      
+            Destroy(this.gameObject);
+       
+     
+    }
+}
