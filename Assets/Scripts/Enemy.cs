b6dd92076ca946e9944e8d3ad132ce48cd08ae4a
Boredest <hodge195@gmail.com>
2022-06-01 20:15:28 -0700

    Fixed stars in background. Started object pool and enemy behavior scripts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int sprArrSize = 4;
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    private int randomIndex;
+   
+
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        randomIndex = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[randomIndex];
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.down * Time.deltaTime;
+    }
+
+    //todo
+    //col
+    //shooting
+    //hp?
+}
---
+++ Assets/Scripts/Object_Pool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Pool : MonoBehaviour
+{
+   
+    private int poolStartSize = 5;
+    public GameObject enemyPrefab;
+    public Queue<GameObject> enemyPool = new Queue<GameObject>();
+
+
+   private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
